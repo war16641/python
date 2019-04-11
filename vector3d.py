180336a30fe8272ce91f8d3b2043331cdfb97072
war16641 <44914209+war16641@users.noreply.github.com>
2019-04-11 09:31:01 +0800

    Update vector3d.py

--- vector3d.py
+++ vector3d.py
@@ -4,6 +4,7 @@ import copy
 from typing import TypeVar, Generic
 import numpy
 from enum import Enum, unique
+import nose.tools
 
 T_Vector = TypeVar('T_Vector')
 T_Plane = TypeVar('T_Plane')
@@ -148,11 +149,15 @@ class Vector3D(Generic[T_Vector]):
     @modulus.setter
     def modulus(self, v: float) -> None:
         assert not (self.x == 0 and self.y == 0 and self.z == 0)  # 零向量不能设定模
-        assert v >= 0  # 模不能为负
+        # assert v >= 0  # 模不能为负
         t = self.modulus
-        self.x *= v / t
-        self.y *= v / t
-        self.z *= v / t
+        sign = 1
+        if v < 0:
+            sign = -1  # 当v模为负时，向量反向
+            v *= -1
+        self.x *= v / t * sign
+        self.y *= v / t * sign
+        self.z *= v / t * sign
 
     @staticmethod
     def cross_product(v1, v2) -> T_Vector:
@@ -172,18 +177,18 @@ class Vector3D(Generic[T_Vector]):
 
 class Plane3D(Generic[T_Plane]):
     """平面"""
-    @unique
-    class PositionRelationForPlane(Enum): # 面与面的位置关系 同样一下定义是严格互斥 不同于数学上的定义
-        coplane=1
-        parallel=2
-        perpendicular=3
-        skew=4
 
+    @unique
+    class PositionRelationForPlane(Enum):  # 面与面的位置关系 同样一下定义是严格互斥 不同于数学上的定义
+        coplane = 1
+        parallel = 2
+        perpendicular = 3
+        skew = 4
 
     @unique
     class PositionRelationForLine(Enum):
-        include=1
-        parallel=2
+        include = 1
+        parallel = 2
         perpendicular = 3
         skew = 4
 
@@ -201,7 +206,7 @@ class Plane3D(Generic[T_Plane]):
     def __str__(self) -> str:
         return "normal:%s point:%s" % (self.normal, self.point)
 
-    def __contains__(self, item: (T_Vector,T_Line)) -> bool:
+    def __contains__(self, item: (T_Vector, T_Line)) -> bool:
         """
         判断点、线是否在平面上
         :param item:
@@ -222,44 +227,91 @@ class Plane3D(Generic[T_Plane]):
         else:
             raise Exception("type error")
 
-    def __judge_pr_plane(self,pl:T_Plane)->PositionRelationForPlane:
+    def __judge_pr_plane(self, pl: T_Plane) -> PositionRelationForPlane:
         """
         判断面与面的位置关系 只能内部调用
         :param pl:
         :return:
         """
-        if Vector3D.is_parallel(self.normal,pl.normal): #至少平行
-            tmp1=self.point-pl.point
+        if Vector3D.is_parallel(self.normal, pl.normal):  # 至少平行
+            tmp1 = self.point - pl.point
             if tmp1.is_perpendicular(self.normal):
                 return self.PositionRelationForPlane.coplane
             else:
                 return self.PositionRelationForPlane.parallel
-        else: # 相交了
+        else:  # 相交了
             if self.normal.is_perpendicular(pl.normal):
                 return self.PositionRelationForPlane.perpendicular
             else:
                 return self.PositionRelationForPlane.skew
 
-    def __judge_pr_line(self,elo:T_Line)->PositionRelationForLine:
-        if self.normal.is_perpendicular(elo.direction):#至少平行
+    def __judge_pr_line(self, elo: T_Line) -> PositionRelationForLine:
+        if self.normal.is_perpendicular(elo.direction):  # 至少平行
             if elo.point in self:
                 return self.PositionRelationForLine.include
             else:
                 return self.PositionRelationForLine.parallel
-        else:#非平行
-            if Vector3D.is_parallel(self.normal,elo.direction):
+        else:  # 非平行
+            if Vector3D.is_parallel(self.normal, elo.direction):
                 return self.PositionRelationForLine.perpendicular
             else:
                 return self.PositionRelationForLine.skew
 
-    def judge_position_relation(self,item:(T_Line,T_Plane))->Enum:
-        if isinstance(item,Plane3D):
+    def judge_position_relation(self, item: (T_Line, T_Plane)) -> Enum:
+        if isinstance(item, Plane3D):
             return self.__judge_pr_plane(item)
-        elif isinstance(item,Line3D):
+        elif isinstance(item, Line3D):
             return self.__judge_pr_line(item)
         else:
             raise Exception("type error")
 
+    def angle(self, other: (T_Plane, T_Line)) -> float:
+        """
+        计算两个平面夹角
+        :param other:
+        :return: 范围：0,pi 与平面  0,pi/2 与直线
+        """
+        if isinstance(other, Plane3D):
+            return math.pi - Vector3D.angle(self.normal, other.normal)
+        elif isinstance(other, Line3D):
+            # 要求直线与平面的关系必须为斜交或者垂直
+            assert self.judge_position_relation(other) in (self.PositionRelationForLine.skew, \
+                                                           self.PositionRelationForLine.perpendicular)
+            alpha = Vector3D.angle(self.normal, other.direction)
+            if alpha < math.pi / 2:
+                return math.pi / 2 - alpha
+            else:
+                return alpha - math.pi / 2
+        else:
+            raise Exception("type error")
+
+    def projection(self, item: (T_Line, T_Vector)) -> T_Line:
+        """
+        直线elo在此平面内的投影所得的直线
+        elo不能垂直于平面
+        :param elo:
+        :return:
+        """
+        if isinstance(item, Vector3D):
+            return self.__projection_point(item)
+        elif isinstance(item, Line3D):
+            pass
+        else:
+            raise Exception("type error")
+
+    def __projection_point(self, pt: T_Vector) -> T_Vector:
+        """
+        空间点在平面上的投影
+        :param pt:
+        :return:
+        """
+        assert isinstance(pt, Vector3D)
+        n = self.normal.get_standard_copy()
+        a = pt - self.point
+        m = a * n
+        n.modulus = m
+        b = a - n
+        return self.point + b
 
 
 class Line3D(Generic[T_Line]):
@@ -341,6 +393,16 @@ class Line3D(Generic[T_Line]):
         else:  # 不相交
             return self.PositionRelation.non_intersect
 
+    def angle(self, other: T_Line) -> float:
+        """
+        与另一直线的夹角 调用Vector.angle实现
+        当两线不在统一平面内时 将两线投影到平面（以公垂线为法线的平面）形成的角度
+        :param other:
+        :return: 位于0到pi之间
+        """
+        assert isinstance(other, Line3D)
+        return Vector3D.angle(self.direction, other.direction)
+
 
 if __name__ == '__main__':
     # 测试开始
@@ -372,12 +434,12 @@ if __name__ == '__main__':
     c = Vector3D(1, 1.1, 2)
     assert c in p1
     assert a not in p1
-    elo=Line3D(Vector3D(0,1,0),Vector3D(2, -.2, 2))
+    elo = Line3D(Vector3D(0, 1, 0), Vector3D(2, -.2, 2))
     assert elo in p1
 
-    a = Plane3D(normal=Vector3D(0,0,1),point=Vector3D(0,0,0))
-    b=Plane3D(normal=Vector3D(0,0,1),point=Vector3D(10,10,10))
-    assert a.judge_position_relation(b)==a.PositionRelationForPlane.parallel
+    a = Plane3D(normal=Vector3D(0, 0, 1), point=Vector3D(0, 0, 0))
+    b = Plane3D(normal=Vector3D(0, 0, 1), point=Vector3D(10, 10, 10))
+    assert a.judge_position_relation(b) == a.PositionRelationForPlane.parallel
     b = Plane3D(normal=Vector3D(0, 1, 0), point=Vector3D(10, 10, 10))
     assert a.judge_position_relation(b) == a.PositionRelationForPlane.perpendicular
     b = Plane3D(normal=Vector3D(0, 0, 1), point=Vector3D(10, 10, 0))
@@ -385,7 +447,7 @@ if __name__ == '__main__':
     b = Plane3D(normal=Vector3D(1, 0, 1), point=Vector3D(10, 10, 0))
     assert a.judge_position_relation(b) == a.PositionRelationForPlane.skew
 
-    elo=Line3D(direction=Vector3D(0,1,0),point=Vector3D(0,0,0))
+    elo = Line3D(direction=Vector3D(0, 1, 0), point=Vector3D(0, 0, 0))
     assert a.judge_position_relation(elo) is a.PositionRelationForLine.include
     elo = Line3D(direction=Vector3D(0, 1, 0), point=Vector3D(0, 0, 10))
     assert a.judge_position_relation(elo) is a.PositionRelationForLine.parallel
@@ -393,6 +455,25 @@ if __name__ == '__main__':
     assert a.judge_position_relation(elo) is a.PositionRelationForLine.perpendicular
     elo = Line3D(direction=Vector3D(1, 1, 1), point=Vector3D(0, 0, 10))
     assert a.judge_position_relation(elo) is a.PositionRelationForLine.skew
+
+    a = Plane3D(normal=Vector3D(0, 0, 1), point=Vector3D(0, 0, 0))
+    b = Plane3D(normal=Vector3D(0, 1, 0), point=Vector3D(10, 10, 10))
+    assert a.angle(b) == math.pi / 2
+
+    a = Plane3D(normal=Vector3D(0, 0, 1), point=Vector3D(0, 0, 0))
+    elo = Line3D(direction=Vector3D(0, 0, -1), point=Vector3D())
+    assert a.angle(elo) == math.pi / 2
+    elo = Line3D(direction=Vector3D(0, 1, 1), point=Vector3D())
+    assert abs(a.angle(elo) - math.pi / 4) < Vector3D.tol_for_eq
+    elo = Line3D(direction=Vector3D(0, 1, 0), point=Vector3D())
+    nose.tools.assert_raises(AssertionError, a.angle, elo)
+
+    a = Plane3D(normal=Vector3D(0, 0, 1), point=Vector3D(0, 0, 0))
+    pt = Vector3D(10, 10, 0)
+    assert a.projection(pt) == pt
+    b = Plane3D(normal=Vector3D(0, 0, 1), point=Vector3D(0, 0, 2))
+    assert b.projection(pt) == Vector3D(10, 10, 2)
+
     # 测试结束
 
     # 测试开始 直线
@@ -410,4 +491,8 @@ if __name__ == '__main__':
     assert a.judge_position_relation(b) == a.PositionRelation.non_intersect
     c = Line3D(Vector3D(-1.3, 0, 0), Vector3D(111, 0, 0))
     assert a.judge_position_relation(c) == a.PositionRelation.collinear
+
+    a = Line3D(Vector3D(1, 0, 0))
+    b = Line3D(Vector3D(0, 1, 0), Vector3D(0, 0, 4))
+    assert a.angle(b) == math.pi / 2
     # 测试结束
