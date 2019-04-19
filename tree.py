01bd50baa74d9b4e4a6b1aa3674e0b6c39b8fad7
war16641 <1627380717@qq.com>
2019-04-19 14:45:41 +0800

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,25 +1,30 @@
 from typing import TypeVar, Generic,List
 T_Node=TypeVar('T_Node')
+T_Tree=TypeVar('T_Tree')
 rootNode='root'#根节点标志 这个值不重要
-class Node:
-    pass
+
 
 class Node(Generic[T_Node]):
     """
     树上的节点
     节点实例化通过：调用tree中make_root_node方法生成根节点 或 内部addchild方法
     """
-    def __init__(self,parent,data=None):
+    def __init__(self,parent,data=None,tree:T_Tree=None):
         if not( isinstance(parent,Node) or parent == rootNode):
             raise Exception("parent参数不合法")
+        assert isinstance(tree,Tree)
         self.parent=parent
         self.data=data
         self.child_nodes=[]#子节点 初始化为空列表
+        self.tree=tree
         #处理节点的深度
         if parent==rootNode:
-            self.depth=1
+            self.depth=0
         else:
             self.depth=1+parent.depth
+        #新增节点的深度可能会影响树的高度
+        self.tree.updata_height(self.depth)
+
 
     @property
     def degree(self):#节点的度
@@ -31,13 +36,26 @@ class Node(Generic[T_Node]):
         :param data: 是子节点的数据 不是node类
         :return: 生成的子节点
         """
-        cd=Node(parent=self,data=data)
+        cd=Node(parent=self,
+                data=data,
+                tree=self.tree)
         self.child_nodes.append(cd)
         return cd
 
+    def get_siblings(self,)->List[T_Node]:
+        """
+        返回siblings
+        :return: 没有就返回[]
+        """
+        if self.parent==rootNode:
+            return []
+        siblings=self.parent.child_nodes
+        siblings=[x for x in siblings if x!=self]
+        return siblings
 
 
-class Tree:
+
+class Tree(Generic[T_Tree]):
     """
     树 数据结构
     来源：https://en.wikipedia.org/wiki/Tree_(data_structure)
@@ -48,15 +66,19 @@ class Tree:
     """
     def __init__(self):
         self.root_node=None
+        self._height=-1#在没有节点时高度为-1
         pass
 
     def make_root_node(self,data)->T_Node:
         """
+        生成根节点
         #返回生成的节点
         :param data: 节点数据
         :return:
         """
-        self.root_node=Node(rootNode,data)
+        self.root_node=Node(parent=rootNode,
+                            data=data,
+                            tree=self)
         return self.root_node
 
 
@@ -94,6 +116,8 @@ class Tree:
         search_in_child_nodes([self.root_node],lst)
         return lst
 
+
+
     def get_path(self,ancestor:T_Node,descendent:T_Node)->list:
         """
         寻找从一个节点到另一个节点的路
@@ -116,22 +140,38 @@ class Tree:
                 path.append(up)
                 up=up.parent
 
+    @property
+    def height(self):
+        return self._height
+
+    def updata_height(self,a_depth:int)->Node:
+        """
+        更新树的高度 =节点的最大深度
+        :param a_depth:
+        :return:
+        """
+        self._height=a_depth if a_depth>self._height else self._height
+
 
 
 
 if __name__ == '__main__':
     tree=Tree()
+    assert tree.height == -1
     rn=tree.make_root_node(data='0')
+    assert tree.height == 0
     n1=rn.add_child('01')
+    assert tree.height == 1
     n2=rn.add_child('02')
-    n1.add_child('011')
+    assert tree.height == 1
+    t1=n1.add_child('011')
     n1.add_child('012')
     n1.add_child('013')
     n2.add_child('021')
     n2.add_child('022')
     assert len(tree.get_all_leafs())==5
     assert len(tree.get_all_nodes())==8
+    assert len(t1.get_siblings())==2
+    assert len(rn.get_siblings())==0
+    assert tree.height==2
 
-    tree = Tree()
-    rn = tree.make_root_node(data='0')
-    tree.get_all_leafs()[0]
