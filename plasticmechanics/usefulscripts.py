441c9c4097a71b920a0f34e356fd8d0f97bf045e
war16641 <1627380717@qq.com>
2019-04-23 13:11:43 +0800

    塑性力学相关的函数

---
+++ plasticmechanics/usefulscripts.py
@@ -0,0 +1,55 @@
+"""一些常用的命令"""
+import numpy as np
+
+def eigenvalue_problem(A:np.ndarray)->(np.ndarray,np.ndarray):
+    """
+    求方阵的特征向量 特征值
+    :param A:
+    :return: eigenvalue按从大到小排列 形状为1*多列
+             eigenvector与eigenvalue一一对应 每一列为一个特征向量
+    """
+    assert isinstance(A,np.ndarray)
+    (m,n)=A.shape
+    assert m==n,'必须要求是方阵'
+    eigenvalue,eigenvector=np.linalg.eig(A)
+    tmp=eigenvalue.argsort()
+    eigenvalue=eigenvalue[tmp[::-1]]
+    eigenvector=eigenvector[:,tmp[::-1]]
+    return eigenvalue,eigenvector
+
+def stress_solver(stress:np.ndarray)->():
+    """
+    对一个应力张量进行分析
+    :param stress:
+    :return: pricipal_stress,mean_normal_stress,stress_deviator,invariant_of_deviator
+            pricipal_stress 三个主应力
+            mean_normal_stress 平均正应力
+            stress_deviator 应力偏量
+            invariant_of_deviator 应力偏量的三个不变量
+    """
+    assert isinstance(stress, np.ndarray)
+    pricipal_stress, _=eigenvalue_problem(stress)
+    mean_normal_stress=1/3*pricipal_stress.sum()#平均正应力
+    stress_deviator=stress-mean_normal_stress*np.eye(3)#应力偏量
+    eigenvalue,_=eigenvalue_problem(stress_deviator)
+    invariant_of_deviator=[0,0,0]
+    invariant_of_deviator[0]=eigenvalue.sum()
+    invariant_of_deviator[1] = -1*(eigenvalue[0]*eigenvalue[1]+eigenvalue[0]*eigenvalue[2]+eigenvalue[1]*eigenvalue[2])
+    invariant_of_deviator[2]=eigenvalue[0]*eigenvalue[1]*eigenvalue[2]
+    return pricipal_stress,mean_normal_stress,stress_deviator,invariant_of_deviator
+
+
+if __name__ == '__main__':
+    # A = np.array([[3/2,-1/2/2**0.5,-1/2/2**0.5],
+    #               [-1/2/2**0.5, 11/4, -5/4],
+    #               [-1/2/2**0.5,-5/4,11/4]])
+    # stress_solver(A)
+
+    A = np.array([[-2,0,0],
+                  [0,-1,0],
+                  [0,0,-1]])
+    pricipal_stress,mean_normal_stress,stress_deviator,invariant_of_deviator=stress_solver(A)
+    print(invariant_of_deviator)
+    A = 2*A
+    pricipal_stress, mean_normal_stress, stress_deviator, invariant_of_deviator = stress_solver(A)
+    print(invariant_of_deviator)
