ca1c8ccc9401fa0fda477dc44969ad184daca676
war16641 <1627380717@qq.com>
2019-05-06 21:58:22 +0800

    Update usefulscripts.py

--- plasticmechanics/usefulscripts.py
+++ plasticmechanics/usefulscripts.py
@@ -1,5 +1,6 @@
 """一些常用的命令"""
 import numpy as np
+from GoodToolPython.vector3d import Vector3D
 
 def eigenvalue_problem(A:np.ndarray)->(np.ndarray,np.ndarray):
     """
@@ -45,11 +46,38 @@ if __name__ == '__main__':
     #               [-1/2/2**0.5,-5/4,11/4]])
     # stress_solver(A)
 
-    A = np.array([[-2,0,0],
-                  [0,-1,0],
-                  [0,0,-1]])
-    pricipal_stress,mean_normal_stress,stress_deviator,invariant_of_deviator=stress_solver(A)
-    print(invariant_of_deviator)
-    A = 2*A
-    pricipal_stress, mean_normal_stress, stress_deviator, invariant_of_deviator = stress_solver(A)
-    print(invariant_of_deviator)
+
+    A = np.array([[-10,9,5],
+                  [9,0,0],
+                  [5,0,8]])
+    eigenvalue, eigenvector=eigenvalue_problem(A)
+    print(eigenvalue)
+    print(eigenvector)
+    d1=eigenvector[:,0]
+    d2=eigenvector[:,1]
+    d3=eigenvector[:,2]
+    print(np.mat(d1.reshape(1,3)*np.mat(A)*np.mat(d1.reshape(3,1))))
+    d1=Vector3D(d1[0],d1[1],d1[2])
+    d2 = Vector3D(d2[0], d2[1], d2[2])
+    d3 = Vector3D(d3[0], d3[1], d3[2])
+    pass
+
+
+    # pricipal_stress,mean_normal_stress,stress_deviator,invariant_of_deviator=stress_solver(A)
+    # pass
+    # A = np.array([[-2,0,0],
+    #               [0,-1,0],
+    #               [0,0,-1]])
+    # pricipal_stress,mean_normal_stress,stress_deviator,invariant_of_deviator=stress_solver(A)
+    # print(invariant_of_deviator)
+    # A = 2*A
+    # pricipal_stress, mean_normal_stress, stress_deviator, invariant_of_deviator = stress_solver(A)
+    # print(invariant_of_deviator)
+
+    # A = np.array([[-80,16,26],
+    #               [16,26,-28],
+    #               [26,-28,-36]])
+    # d=np.array([0.25,0.5,11**0.5/4])
+    # d=d.reshape(3,1)
+    # print(np.mat(d.reshape(1,3))*np.mat(A)*np.mat(d.reshape(3,1)))
+    # pass
