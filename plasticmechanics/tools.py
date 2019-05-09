bdadfe8bf5deaf403e1ab2874e79db04a710342b
war16641 <44914209+war16641@users.noreply.github.com>
2019-05-09 23:36:44 +0800

    Create tools.py

---
+++ plasticmechanics/tools.py
@@ -0,0 +1,30 @@
+"""
+这个包放置一些工具函数
+"""
+from GoodToolPython.vector3d import Vector3D
+from typing import Union
+def is_sequence_with_one_type(sequence:Union[Vector3D,list], tp:type)->bool:
+    """
+    判断sequence是否是某一个类型的对象组成的序列
+    :param sequence:序列
+    :param tp:类型
+    :return:
+    """
+    # assert isinstance(sequence,(list,tuple)),'sequnce必须为元组或者列表'
+    if not isinstance(sequence,(list,tuple)):#sequnce必须为元组或者列表,否则返回false
+        return False
+    assert isinstance(tp,type),'tp必须为类'
+
+    for i in sequence:
+        if not isinstance(i,tp):
+            return False
+    return True
+
+if __name__ == '__main__':
+    assert not is_sequence_with_one_type(1,Vector3D)
+    v1=Vector3D()
+    v2=Vector3D(1)
+    i1=1.
+    i2=2.
+    assert is_sequence_with_one_type([i1,i2],float)
+    assert not is_sequence_with_one_type([i1, i2,v1], float)
