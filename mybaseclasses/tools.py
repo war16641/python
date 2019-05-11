34d9cefeac414e67e8ed43b553fb61fd68dc220f
war16641 <44914209+war16641@users.noreply.github.com>
2019-05-11 21:30:57 +0800

    Update tools.py

--- mybaseclasses/tools.py
+++ mybaseclasses/tools.py
@@ -3,16 +3,18 @@
 """
 from GoodToolPython.vector3d import Vector3D
 from typing import Union
-def is_sequence_with_one_type(sequence:Union[Vector3D,list], tp:type)->bool:
+def is_sequence_with_one_type(sequence:Union[Vector3D,list], tp:type=None)->bool:
     """
     判断sequence是否是某一个类型的对象组成的序列
     :param sequence:序列
-    :param tp:类型
+    :param tp:类型 如果是none 则取第一个元素的类型
     :return:
     """
     # assert isinstance(sequence,(list,tuple)),'sequnce必须为元组或者列表'
     if not isinstance(sequence,(list,tuple)):#sequnce必须为元组或者列表,否则返回false
         return False
+    if tp is None:
+        tp=type(sequence[0])
     assert isinstance(tp,type),'tp必须为类'
 
     for i in sequence:
