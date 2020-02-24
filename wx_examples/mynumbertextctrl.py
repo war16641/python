8573ffd2a3d46cf29bc34840c7bb992b984ccbcc
war16641 <44914209+war16641@users.noreply.github.com>
2020-02-24 13:28:18 +0800

    Update mynumbertextctrl.py

--- wx_examples/mynumbertextctrl.py
+++ wx_examples/mynumbertextctrl.py
@@ -10,16 +10,17 @@ class MyNumberTextCtrl(wx.TextCtrl):
     重载的value属性 可通过他读取设置值：
     """
 
-    def __init__(self, parent, value_on_invalid=0, *args, **kwargs):
+    def __init__(self, parent, value_on_invalid=0,text_on_invalid=0, *args, **kwargs):
         super().__init__(parent, validator=MyNumberValidator(), *args, **kwargs)
         self.value_on_invalid = value_on_invalid  # 当读取的不为数时 返回的值，通常为0或者none
+        self.text_on_invalid=text_on_invalid#当读取的不为数时，文本框内设置的值
 
     @property
     def Value(self) -> float:  # 获取数字
         t = super().Value
         f, v = is_number(t)
         if f is False:  # 非法数
-            super().ChangeValue("非法数")
+            super().ChangeValue(str(self.text_on_invalid))
             return self.value_on_invalid
         else:
             return v
@@ -59,8 +60,9 @@ class Frame(wx.Frame):
     def st(self, evt):
         self.tc2.Value = self.tc1.Value
 
+if __name__ == '__main__':
 
-app = wx.App()
-fr = Frame()
-fr.Show()
-app.MainLoop()
+    app = wx.App()
+    fr = Frame()
+    fr.Show()
+    app.MainLoop()
